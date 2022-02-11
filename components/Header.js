96db300b9c9569bd6767d23e4ce3bc9deaa696b8
Philip Kvam <ph_kvam@hotmail.com>
2022-02-11 19:01:00 +0100

    added animation falcon9

--- components/Header.js
+++ components/Header.js
@@ -7,16 +7,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 
-
-
 //Animate
-const containerHeader= {
+const containerHeader = {
   show: {
     transition: {
       staggerChildren: 1,
     },
   },
 };
+
 
 const itemHeader = {
   hidden: {
@@ -65,13 +64,10 @@ const itemLogo = {
 const itemLogoMenu = {
   hidden: {
     opacity: 0,
-   
- 
   },
   show: {
     opacity: 1,
-   
- 
+
     transition: {
       ease: [0.6, 0.01, -0.05, 0.95],
       duration: 1.6,
@@ -79,39 +75,14 @@ const itemLogoMenu = {
   },
   exit: {
     opacity: 0,
-  
-   
+
     transition: {
       ease: "easeInOut",
       duration: 0.8,
     },
   },
 };
-const itemMenuLink = {
-  hidden: {
-    opacity: 0,
-   x:-200
- 
-  },
-  show: {
-    opacity: 1,
-    x: 0,
- 
-    transition: {
-      ease: [0.6, 0.01, -0.05, 0.95],
-      duration: 1.6,
-    },
-  },
-  exit: {
-    opacity: 0,
-    x:-200,
-   
-    transition: {
-      ease: "easeInOut",
-      duration: 0.8,
-    },
-  },
-};
+
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
   const router = useRouter();
@@ -126,85 +97,79 @@ export default function Header() {
     return () => router.events.off("routeChangeStart", hide);
   }, [hide, router.events]);
 
-
   return (
     <>
       <header>
         <motion.div
-         variants={containerHeader}
-         initial="hidden"
-         animate="show"
-         exit="exit"
+          variants={containerHeader}
+          initial="hidden"
+          animate="show"
+          exit="exit"
         >
-        <nav>
-        <motion.div variants={itemHeader} >
-          <div className="nav__content">
-          
-            <FontAwesomeIcon
-              className="hamburger"
-              icon={faBars}
-              size="2x"
-              onClick={() => setShowMenu(!showMenu)}
-            />
-          
-               
-            {showMenu && (
-             
-                
-              <div className="menu-show">
-                <div className="close" onClick={() => setShowMenu(!showMenu)}>
-                  Close
-                </div>
-              
-                <ul>
-               
-                  <li>
-                    <Link href="/">Home</Link>
-                  </li>
-                
-                  <li>
-                    <Link href="/falcon9">Falcon 9</Link>
-                  </li>
-                
-                  <li>
-                    <Link href="/falconHeavy">Falcon Heavy</Link>
-                  </li>
-               
-                  <li>
-                    <Link href="/starship">Starship</Link>
-                  </li>
+          <nav>
+            <motion.div variants={itemHeader}>
+              <div className="nav__content">
+                <FontAwesomeIcon
+                  className="hamburger"
+                  icon={faBars}
+                  size="2x"
+                  onClick={() => setShowMenu(!showMenu)}
+                />
+
+                {showMenu && (
                   
-                  <li>
-                    <Link href="/history">History</Link>
-                  </li>
+                  <div className="menu-show">
+                    <div
+                      className="close"
+                      onClick={() => setShowMenu(!showMenu)}
+                    >
+                      Close
+                    </div>
+
+                    <ul>
+                      <li>
+                        <Link href="/">Home</Link>
+                      </li>
+
+                      <li>
+                        <Link href="/falcon9">Falcon 9</Link>
+                      </li>
+
+                      <li>
+                        <Link href="/falconHeavy">Falcon Heavy</Link>
+                      </li>
+
+                      <li>
+                        <Link href="/starship">Starship</Link>
+                      </li>
+
+                      <li>
+                        <Link href="/history">History</Link>
+                      </li>
+
+                      <li>
+                        <Link href="/contact">Contact</Link>
+                      </li>
+                    </ul>
+
+                    <motion.div variants={itemLogoMenu}>
+                      <div className="container__logo">
+                        <Image src={Logo} width="200" />
+                      </div>
+                    </motion.div>
+                  </div>
                   
-                  <li>
-                    <Link href="/contact">Contact</Link>
-                  </li>
-                  
-                 
-                </ul>
+                )}
                
-                
-                <motion.div variants={itemLogoMenu} >
-                <div className="container__logo">
-                  <Image src={Logo} width="200" />
-                </div>
-                </motion.div>
               </div>
-             
-             
-            )}
-           
-          </div>
-          
+              
+            </motion.div>
+          </nav>
+          <motion.div variants={itemLogo}>
+            <div className="container__logo">
+              <Image src={Logo} width="200" />
+            </div>
           </motion.div>
-        </nav>
-        <motion.div variants={itemLogo} >
-        <div className="container__logo">
-          <Image src={Logo} width="200" />
-        </div>
-        </motion.div>
         </motion.div>
       </header>
     </>
--- pages/falcon9.js
+++ pages/falcon9.js
@@ -2,6 +2,66 @@ import Image from "next/image";
 import falcon9launch from "../images/falcon9launch-min_ccexpress-min.jpeg";
 import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { motion } from "framer-motion";
+
+//Animate
+
+//Hero
+const containerHero = {
+  show: {
+    transition: {
+      staggerChildren: 1,
+    },
+  },
+};
+
+const itemHero = {
+  hidden: {
+    opacity: 0,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.8,
+    },
+  },
+};
+const itemHeroImg = {
+  hidden: {
+    opacity: 0,
+    scale: 0.2,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 1,
+    y: -200,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.8,
+    },
+  },
+};
 
 export default function Falcon9({ rocket }) {
   console.log(rocket);
@@ -10,160 +70,365 @@ export default function Falcon9({ rocket }) {
 
   return (
     <div className="main">
-      <div className="content">
+      <motion.div
+        className="content"
+        variants={containerHero}
+        initial="hidden"
+        animate="show"
+        exit="exit"
+      >
         <section className="feature">
           <div className="container__image falcon9">
-            {/* <Image src={falcon9launch} 
-            placeholder="blur"
-            /> */}
+            <motion.div variants={itemHeroImg}>
+              <div className="background falcon9"></div>
+            </motion.div>
             <div className="feature__heading">
-            <h1>{rocket.name}</h1>
-            <h2>First orbital class rocket capable of reflight</h2>
+              <motion.div variants={itemHero}>
+                <h1>{rocket.name}</h1>
+              </motion.div>
+              <motion.div variants={itemHero}>
+                <h2>First orbital class rocket capable of reflight</h2>
+              </motion.div>
             </div>
-           
           </div>
         </section>
         <section className="stats">
           <div className="container__stats">
-            <div className="row">
-              <span>Cost per launch:</span>
-              <span>${rocket.cost_per_launch.toLocaleString()}</span>
-            </div>
-            <div className="row">
-              <span>Country:</span>
-              <span>{rocket.country}</span>
-            </div>
-            <div className="row">
-              <span>First flight:</span>
-              <span>{rocket.first_flight}</span>
-            </div>
-            <div className="row description">
-              <span>{rocket.description}</span>
-            </div>
+            <motion.div
+              variants={itemHero}
+              initial="hidden"
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.2,
+                  ease: [0.6, 0.01, -0.05, 0.95],
+                  duration: 1.6,
+                },
+              }}
+              exit="exit"
+            >
+              <div className="row">
+                <span>
+                  <h3>Cost per launch:</h3>
+                  <h3>${rocket.cost_per_launch.toLocaleString()} </h3>
+                </span>
+              </div>
+            </motion.div>
+            <motion.div
+              variants={itemHero}
+              initial="hidden"
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.2,
+                  ease: [0.6, 0.01, -0.05, 0.95],
+                  duration: 1.6,
+                },
+              }}
+              exit="exit"
+            >
+              <div className="row">
+                <span>
+                  <h3>
+                    {" "}
+                    Country:
+                    {rocket.country}
+                  </h3>
+                </span>
+              </div>
+            </motion.div>
+            <motion.div
+              variants={itemHero}
+              initial="hidden"
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.2,
+                  ease: [0.6, 0.01, -0.05, 0.95],
+                  duration: 1.6,
+                },
+              }}
+              exit="exit"
+            >
+              <div className="row">
+                <span>
+                  {" "}
+                  <h3>
+                    {" "}
+                    First flight:
+                    {rocket.first_flight}
+                  </h3>
+                </span>
+              </div>
+            </motion.div>
+            <motion.div
+              variants={itemHero}
+              initial="hidden"
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.2,
+                  ease: [0.6, 0.01, -0.05, 0.95],
+                  duration: 1.6,
+                },
+              }}
+              exit="exit"
+            >
+              <div className="row description">
+                <span>{rocket.description}</span>
+              </div>
+            </motion.div>
           </div>
         </section>
         <section className="overview">
-          <Carousel
-            styles={styles}
-            showThumbs={false}
-            showStatus={false}
-            showIndicators={false}
-            showArrows={true}
-            infiniteLoop={true}
+          <motion.div
+            variants={itemHero}
+            initial="hidden"
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 0.2,
+                ease: [0.6, 0.01, -0.05, 0.95],
+                duration: 1.6,
+              },
+            }}
+            exit="exit"
           >
-            <div className="section-inner">
-              <div className="inner-content">
-                <h2>{rocket.name}</h2>
-                <h3>Overview</h3>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>Height</td>
-                      <td>
-                        {rocket.height.meters} m{" "}
-                        <span>/{rocket.height.feet} ft</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Diameter</td>
-                      <td>
-                        {rocket.diameter.meters} m{" "}
-                        <span>/{rocket.diameter.feet} ft</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Mass</td>
-                      <td>
-                        {rocket.mass.kg.toLocaleString()} kg{" "}
-                        <span>/{rocket.mass.lb.toLocaleString()} lb</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Payload to Leo</td>
-                      <td>
-                        {rocket.payload_weights[0].kg.toLocaleString()} kg{" "}
-                        <span>
-                          /{rocket.payload_weights[0].lb.toLocaleString()} lb
-                        </span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Payload to Gto</td>
-                      <td>
-                        {rocket.payload_weights[1].kg.toLocaleString()} kg{" "}
-                        <span>
-                          /{rocket.payload_weights[1].lb.toLocaleString()} lb
-                        </span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Payload to Mars</td>
-                      <td>
-                        {rocket.payload_weights[2].kg.toLocaleString()} kg{" "}
-                        <span>
-                          /{rocket.payload_weights[2].lb.toLocaleString()} lb
-                        </span>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+            <Carousel
+              styles={styles}
+              showThumbs={false}
+              showStatus={false}
+              showIndicators={false}
+              showArrows={true}
+              infiniteLoop={true}
+            >
+              <div className="section-inner">
+                <div className="inner-content">
+                  <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
+                    <h2>{rocket.name}</h2>
+                    <h3>Overview</h3>
+                  </motion.div>
+                  <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>Height</td>
+                          <td>
+                            {rocket.height.meters} m{" "}
+                            <span>/{rocket.height.feet} ft</span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Diameter</td>
+                          <td>
+                            {rocket.diameter.meters} m{" "}
+                            <span>/{rocket.diameter.feet} ft</span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Mass</td>
+                          <td>
+                            {rocket.mass.kg.toLocaleString()} kg{" "}
+                            <span>/{rocket.mass.lb.toLocaleString()} lb</span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Payload to Leo</td>
+                          <td>
+                            {rocket.payload_weights[0].kg.toLocaleString()} kg{" "}
+                            <span>
+                              /{rocket.payload_weights[0].lb.toLocaleString()}{" "}
+                              lb
+                            </span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Payload to Gto</td>
+                          <td>
+                            {rocket.payload_weights[1].kg.toLocaleString()} kg{" "}
+                            <span>
+                              /{rocket.payload_weights[1].lb.toLocaleString()}{" "}
+                              lb
+                            </span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Payload to Mars</td>
+                          <td>
+                            {rocket.payload_weights[2].kg.toLocaleString()} kg{" "}
+                            <span>
+                              /{rocket.payload_weights[2].lb.toLocaleString()}{" "}
+                              lb
+                            </span>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </motion.div>
+                </div>
               </div>
-            </div>
-            <div className="section-inner">
-              <div className="inner-content">
-                <h2>{rocket.name}</h2>
-                <h3>First stage</h3>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>Burn time</td>
-                      <td>{rocket.first_stage.burn_time_sec} sec</td>
-                    </tr>
-                    <tr>
-                      <td>Engines</td>
-                      <td>{rocket.first_stage.engines} </td>
-                    </tr>
-                    <tr>
-                      <td>Fuel amount</td>
-                      <td>{rocket.first_stage.fuel_amount_tons} tons </td>
-                    </tr>
-                    <tr>
-                      <td>Landing legs</td>
-                      <td>{rocket.landing_legs.material} </td>
-                    </tr>
-                    <tr>
-                      <td>Number of legs</td>
-                      <td>{rocket.landing_legs.number} </td>
-                    </tr>
-                  </tbody>
-                </table>
+              <div className="section-inner">
+                <div className="inner-content">
+                  <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
+                    <h2>{rocket.name}</h2>
+                    <h3>First stage</h3>
+                  </motion.div>
+                  <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>Burn time</td>
+                          <td>{rocket.first_stage.burn_time_sec} sec</td>
+                        </tr>
+                        <tr>
+                          <td>Engines</td>
+                          <td>{rocket.first_stage.engines} </td>
+                        </tr>
+                        <tr>
+                          <td>Fuel amount</td>
+                          <td>{rocket.first_stage.fuel_amount_tons} tons </td>
+                        </tr>
+                        <tr>
+                          <td>Landing legs</td>
+                          <td>{rocket.landing_legs.material} </td>
+                        </tr>
+                        <tr>
+                          <td>Number of legs</td>
+                          <td>{rocket.landing_legs.number} </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </motion.div>
+                </div>
               </div>
-            </div>
-            <div className="section-inner">
-              <div className="inner-content">
-                <h2>{rocket.name}</h2>
-                <h3>Second stage</h3>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>Burn time</td>
-                      <td>{rocket.second_stage.burn_time_sec} sec</td>
-                    </tr>
-                    <tr>
-                      <td>Engines</td>
-                      <td>{rocket.second_stage.engines} </td>
-                    </tr>
-                    <tr>
-                      <td>Fuel amount</td>
-                      <td>{rocket.second_stage.fuel_amount_tons} tons </td>
-                    </tr>
-                  </tbody>
-                </table>
+              <div className="section-inner">
+                <div className="inner-content">
+                  <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
+                    <h2>{rocket.name}</h2>
+                    <h3>Second stage</h3>
+                  </motion.div>
+                  <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>Burn time</td>
+                          <td>{rocket.second_stage.burn_time_sec} sec</td>
+                        </tr>
+                        <tr>
+                          <td>Engines</td>
+                          <td>{rocket.second_stage.engines} </td>
+                        </tr>
+                        <tr>
+                          <td>Fuel amount</td>
+                          <td>{rocket.second_stage.fuel_amount_tons} tons </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </motion.div>
+                </div>
               </div>
-            </div>
-          </Carousel>
+            </Carousel>
+          </motion.div>
         </section>
+        <motion.div
+                    variants={itemHero}
+                    initial="hidden"
+                    whileInView={{
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: 0.2,
+                        ease: [0.6, 0.01, -0.05, 0.95],
+                        duration: 1.6,
+                      },
+                    }}
+                    exit="exit"
+                  >
         <section className="gallery">
+      
           <Carousel
             styles={styles}
             showThumbs={false}
@@ -172,16 +437,21 @@ export default function Falcon9({ rocket }) {
             showArrows={true}
             infiniteLoop={true}
           >
-            {rocket.flickr_images.map((img) => 
-            <div key={imageCarouselKey}>
-              <img src= {img} />
-            </div>
-            )}
           
+            {rocket.flickr_images.map((img) => (
+                
+              <div key={imageCarouselKey}>
+                <img src={img} />
+              </div>
+             
+            ))}
+            
           </Carousel>
+        
+        
         </section>
-      
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
